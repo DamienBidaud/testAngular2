a5aced2243e862bdcc99f38a58e5a0d50d1e9c57
Damien Bidaud <dbdartian@gmail.com>
2016-05-24 11:52:20 +0200

    package.json valid

---
+++ src/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by d.bidaud on 24/05/2016.
+ */
+import {Component} from "@angular/core";
+
+@Component({
+    selector:"my-app",
+    template:"<h1>My First Angular 2 app</h1>"
+})
+export class AppComponent{}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by d.bidaud on 24/05/2016.
+ */
+import 'babel-polyfill';
+import 'zone.js/dist/zone.js';
+
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import {AppComponent} from "./app";
+
+bootstrap(AppComponent);
